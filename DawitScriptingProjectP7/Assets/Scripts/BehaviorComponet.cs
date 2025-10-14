d61bc7c29a0ba0b14d9a2a1640a0da1a037b08dc
PhysicsND <nayakdawit@outlook.com>
2025-10-14 15:28:21 -0700

    made cube and made plane and cube color
    made the colors red blue green cyan clear grey and white

---
+++ DawitScriptingProjectP7/Assets/Scripts/BehaviorComponet.cs
@@ -0,0 +1,50 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BehaviorComponet : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            GetComponent<Renderer>().material.color = Color.red;
+        }
+        if (Input.GetKeyDown(KeyCode.B))
+        {
+            GetComponent<Renderer>().material.color = Color.blue;
+        }
+
+        if (Input.GetKeyDown(KeyCode.G))
+        {
+            GetComponent<Renderer>().material.color = Color.green;
+        }
+
+        if (Input.GetKeyDown(KeyCode.C))
+        {
+            GetComponent<Renderer>().material.color = Color.cyan;
+        }
+
+        if (Input.GetKeyDown(KeyCode.H))
+        {
+            GetComponent<Renderer>().material.color = Color.gray;
+        }
+
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            GetComponent<Renderer>().material.color = Color.white;
+        }
+
+        if (Input.GetKeyDown(KeyCode.A))
+        {
+            GetComponent<Renderer>().material.color = Color.clear;
+        }
+    }
+}
