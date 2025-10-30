da286b09ed6155732c12456dc87c2cac54cb7e52
PhysicsND <nayakdawit@outlook.com>
2025-10-30 14:01:30 -0700

    Varibles and functions
    Created a function to multiply Int by 2

---
+++ DawitScriptingProjectP7/Assets/Scripts/Varibles and fuctions.cs
@@ -0,0 +1,25 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEngine;
+
+public class VariablesAndFunctions : MonoBehaviour
+{
+    int myInt = 5;
+
+
+    void Start()
+    {
+        myInt = MultiplyByTwo(myInt);
+        Debug.Log(myInt);
+    }
+
+
+    int MultiplyByTwo(int number)
+    {
+        int result;
+        result = number * 2;
+        return result;
+    }
+}// creating a funct to mult by 2
