1514eaa184059e46807a435d831a6685f5111776
PhysicsND <nayakdawit@outlook.com>
2025-10-30 14:33:45 -0700

    Conventions and Syntax
    added script and namespace

---
+++ DawitScriptingProjectP7/Assets/Scripts/Basic Syntax.cs
@@ -0,0 +1,19 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BasicSyntax : MonoBehaviour
+{
+    void Start()
+    {
+        //this line is there to tell me the x position of my object
+
+        
+        Debug.Log(transform.position.x);
+
+        if (transform.position.y <= 5f)
+        {
+            Debug.Log("I'm about to hit the ground!");
+        }
+    }
+}
