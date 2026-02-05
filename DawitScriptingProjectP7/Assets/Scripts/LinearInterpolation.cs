6f9ecd05a8861138c21ddd3e5c1da4286c5e697a
PhysicsND <nayakdawit@outlook.com>
2026-02-05 14:24:14 -0800

    Finished linear interpolation

---
+++ DawitScriptingProjectP7/Assets/Scripts/LinearInterpolation.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LinearInterpolation : MonoBehaviour
+{/*
+   void Start()
+    {
+        // Interpolates halfway between 3 and 5
+        float result = Mathf.Lerp(3f, 5f, 0.5f);
+
+        // Creates starting Vector3
+        Vector3 from = new Vector3(1f, 2f, 3f);
+
+        // Creates ending Vector3
+        Vector3 to = new Vector3(5f, 6f, 7f);
+
+        // Interpolates 75% between the two vectors
+        Vector3 vectorResult = Vector3.Lerp(from, to, 0.75f);
+    }
+
+    void Update()
+    {
+        // Smoothly moves a value toward 8 over time
+        float smoothValue = Mathf.Lerp(0f, 8f, 0.5f * Time.deltaTime);
+    }
+    */
+}
