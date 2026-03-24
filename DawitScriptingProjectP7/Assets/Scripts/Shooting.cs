bc466a855a26b8622065851d1ea532f187854ecd
PhysicsND <nayakdawit@outlook.com>
2026-03-24 14:53:24 -0700

    Tank

---
+++ DawitScriptingProjectP7/Assets/Scripts/Shooting.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shooting : MonoBehaviour
+{
+    public Rigidbody bulletPrefab;
+    public Transform firePosition;
+    public float bulletSpeed;
+    // Start is called before the first frame update
+    private Inventory inventory;
+
+
+    void Awake()
+    {
+        inventory = GetComponent<Inventory>();
+    }
+
+
+    void Update()
+    {
+        Shoot();
+    }
+
+
+    void Shoot()
+    {
+        if (Input.GetButtonDown("Fire1") && inventory.myStuff.bullets > 0)
+        {
+            Rigidbody bulletInstance = Instantiate(bulletPrefab, firePosition.position, firePosition.rotation) as Rigidbody;
+            bulletInstance.AddForce(firePosition.forward * bulletSpeed);
+            inventory.myStuff.bullets--;
+        }
+    }
+}
